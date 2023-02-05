93d6e4e862326846cb28513b1f48261fa8f21957
Davilkus Games <davilkusgames@gmail.com>
2023-02-05 20:13:37 +0300

    update #2

---
+++ GOF_Lesson_4M/GOF_Lesson_4M_Observer/WheatherData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_Lesson_4M_Observer
+{
+    public class WheatherData
+    {
+        private List<IObserver> observers;
+
+        private float temp;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
+        {
+            observers = new List<IObserver>();
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(temp, humidity, pressure);
+            }
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+    }
+}
