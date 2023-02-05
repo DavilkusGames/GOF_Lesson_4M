f05da4ffbcfdfa92f03d0ff3d4721d8e6d5d4f4d
Davilkus Games <davilkusgames@gmail.com>
2023-02-05 20:40:45 +0300

    fff

---
+++ GOF_Lesson_4M/GOF_Lesson_4M_Observer/AllConditionsDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_Lesson_4M_Observer
+{
+    internal class AllConditionsDisplay : IObserver, IDisplay
+    {
+        // субъект, на который подписываемся
+        private ISubject weatherData;
+
+        // for display
+        float temp;
+        float humidity;
+        float pressure;
+
+        public AllConditionsDisplay(WheatherData weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Display()
+        {
+            Console.Write($"Temp: {temp}, Humidity: {humidity}, Pressure: {pressure}");
+        }
+
+        public void Update(float temp, float humidity, float pressure)
+        {
+            this.temp = temp;
+            this.humidity = humidity;
+            this.pressure = pressure;
+
+            Display();
+        }
+    }
+}
